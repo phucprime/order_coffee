c43f1e88c9f3033426db234c69ebd419460d787a
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-16 22:25:51 +0700

    add HeaderBar to Home screen

--- screens/Home.js
+++ screens/Home.js
@@ -6,31 +6,19 @@ import {
     StyleSheet
 } from 'react-native';
 
+import { HeaderBar } from "../components";
+
 const Home = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
-
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Rewards")}
-            >
-                <Text>Navigate to Rewards</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Location")}
-            >
-                <Text>Navigate to Locations</Text>
-            </TouchableOpacity>
+            <HeaderBar />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        flex: 1
     }
 })
 
