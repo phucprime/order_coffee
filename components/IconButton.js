78f3193e32cd3315def59f3ad41bc857d708bdd2
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 11:44:24 +0700

    eslint fix

--- components/IconButton.js
+++ components/IconButton.js
@@ -1,34 +1,35 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React from 'react'
 
-import { 
-    TouchableOpacity,
-    Image
-} from "react-native";
+import {
+  TouchableOpacity,
+  Image
+} from 'react-native'
 
-import { COLORS } from "../constants";
+import { COLORS } from '../constants'
 
 const IconButton = ({ containerStyle, iconStyle, icon, onPress }) => {
-    return (
+  return (
         <TouchableOpacity
             style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                ...containerStyle
+              alignItems: 'center',
+              justifyContent: 'center',
+              ...containerStyle
             }}
             onPress={onPress}
         >
-            <Image 
+            <Image
                 source={icon}
                 resizeMode='contain'
                 style={{
-                    width: 25,
-                    height: 25,
-                    tintColor: COLORS.white,
-                    ...iconStyle
+                  width: 25,
+                  height: 25,
+                  tintColor: COLORS.white,
+                  ...iconStyle
                 }}
             />
         </TouchableOpacity>
-    )
+  )
 }
 
-export default IconButton;
+export default IconButton
