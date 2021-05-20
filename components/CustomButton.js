f8574afccef69bb7322f572090608f35592f2de1
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 11:45:17 +0700

    eslint fix

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,41 +1,42 @@
-import React from "react";
-import { 
-    TouchableOpacity,
-    Text
-} from "react-native";
+/* eslint-disable react/prop-types */
+import React from 'react'
+import {
+  TouchableOpacity,
+  Text
+} from 'react-native'
 
-import { COLORS } from "../constants";
+import { COLORS } from '../constants'
 
-const CustomButton = ({ 
-    containerStyle, 
-    labelStyle, 
-    label, 
-    onPress, 
-    isPrimaryButton, 
-    isSecondaryButton 
+const CustomButton = ({
+  containerStyle,
+  labelStyle,
+  label,
+  onPress,
+  isPrimaryButton,
+  isSecondaryButton
 }) => {
-    return (
+  return (
         <TouchableOpacity
             style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: isPrimaryButton ? COLORS.primary : COLORS.transparent,
-                borderColor: isSecondaryButton ? COLORS.primary : COLORS.transparent,
-                borderWidth: isSecondaryButton ? 1 : 0,
-                ...containerStyle
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: isPrimaryButton ? COLORS.primary : COLORS.transparent,
+              borderColor: isSecondaryButton ? COLORS.primary : COLORS.transparent,
+              borderWidth: isSecondaryButton ? 1 : 0,
+              ...containerStyle
             }}
             onPress={onPress}
         >
             <Text
                 style={{
-                    color: isPrimaryButton ? COLORS.white : COLORS.primary,
-                    ...labelStyle
+                  color: isPrimaryButton ? COLORS.white : COLORS.primary,
+                  ...labelStyle
                 }}
             >
                 {label}
             </Text>
         </TouchableOpacity>
-    )
+  )
 }
 
-export default CustomButton;
+export default CustomButton
