648c4de59b06b0e562a67c0ee9a05f3304b207b3
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 13:12:41 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,37 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    es6: true
+    es6: true,
+    node: true
   },
   extends: [
     'plugin:react/recommended',
     'standard',
+    'standard-jsx',
+    'standard-react',
     'eslint:recommended'
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: 12
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
   plugins: [
     'react'
   ],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'no-use-before-define': 'off',
+    'no-unused-expressions': 'off',
+    'no-useless-escape': 'off',
+    'generator-star-spacing': ['error', {
+      before: false, after: true
+    }],
+    'yield-star-spacing': ['error', 'after'],
+    camelcase: 'off'
   }
 }
