8920d416862d9763e3ed1addb76ddd9af86dcd63
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 11:42:41 +0700

    eslint fix

--- constants/dummy.js
+++ constants/dummy.js
@@ -1,238 +1,238 @@
 export const availableRewards = [
-    {
-        id: 1,
-        title: "50 points - FREE UP SIZE",
-        eligible: true,
-    },
-    {
-        id: 2,
-        title: "340 points - 35.000đ",
-        eligible: false,
-    },
-    {
-        id: 3,
-        title: "400 points - BUY 1 GET 1 FREE",
-        eligible: false,
-    },
-    {
-        id: 4,
-        title: "500 points - 1 FREE DRINK",
-        eligible: false,
-    },
+  {
+    id: 1,
+    title: '50 points - FREE UP SIZE',
+    eligible: true
+  },
+  {
+    id: 2,
+    title: '340 points - 35.000đ',
+    eligible: false
+  },
+  {
+    id: 3,
+    title: '400 points - BUY 1 GET 1 FREE',
+    eligible: false
+  },
+  {
+    id: 4,
+    title: '500 points - 1 FREE DRINK',
+    eligible: false
+  }
 ]
 
 export const locations = [
-    {
-        id: 1,
-        title: "Bitexco Financial Tower",
-        address: "2 Hai Trieu, Ben Nghe, 1 District, Ho Chi Minh",
-        operation_hours: "Monday - Friday: 08AM - 05PM \nSaturday - Saturday: 10AM - Midnight",
-        bookmarked: true
-    },
-    {
-        id: 2,
-        title: "Landmark 81 Skyscraper",
-        address: "720A Dien Bien Phu, Binh Thanh, Ho Chi Minh",
-        operation_hours: "Sunday - Thursday: 10AM - 11PM \nFriday - Saturday: 10AM - Midnight",
-        bookmarked: false
-    },
-    {
-        id: 3,
-        title: "Keangnam Tower",
-        address: "Pham Hung, Me Tri, Nam Tu Liem, Ha Noi",
-        operation_hours: "Sunday - Thursday: 10AM - 11PM \nFriday - Saturday: 10AM - Midnight",
-        bookmarked: true
-    },
-    {
-        id: 4,
-        title: "Lotte Center",
-        address: "Ba Dinh, Ha Noi",
-        operation_hours: "Sunday - Thursday: 10AM - 11PM \nFriday - Saturday: 10AM - Midnight",
-        bookmarked: false
-    },
+  {
+    id: 1,
+    title: 'Bitexco Financial Tower',
+    address: '2 Hai Trieu, Ben Nghe, 1 District, Ho Chi Minh',
+    operation_hours: 'Monday - Friday: 08AM - 05PM \nSaturday - Saturday: 10AM - Midnight',
+    bookmarked: true
+  },
+  {
+    id: 2,
+    title: 'Landmark 81 Skyscraper',
+    address: '720A Dien Bien Phu, Binh Thanh, Ho Chi Minh',
+    operation_hours: 'Sunday - Thursday: 10AM - 11PM \nFriday - Saturday: 10AM - Midnight',
+    bookmarked: false
+  },
+  {
+    id: 3,
+    title: 'Keangnam Tower',
+    address: 'Pham Hung, Me Tri, Nam Tu Liem, Ha Noi',
+    operation_hours: 'Sunday - Thursday: 10AM - 11PM \nFriday - Saturday: 10AM - Midnight',
+    bookmarked: true
+  },
+  {
+    id: 4,
+    title: 'Lotte Center',
+    address: 'Ba Dinh, Ha Noi',
+    operation_hours: 'Sunday - Thursday: 10AM - 11PM \nFriday - Saturday: 10AM - Midnight',
+    bookmarked: false
+  }
 ]
 
 export const menuList = [
-    {
-        id: 1,
-        name: "Brown Sugar Fresh Milk Tea",
-        description: "This drink is made from 3 ingredients, caramelised brown sugar and milk.",
-        price: 35000,
-        thumbnail: require("../assets/images/brown-sugar-fresh-mt.png"),
-        category: "Milk Tea"
-    },
-    {
-        id: 2,
-        name: "Matcha Milk Tea",
-        description: "In a glass jar, mix the matcha green tea powder with a little bit of water.",
-        price: 39000,
-        thumbnail: require("../assets/images/matcha-mt.png"),
-        category: "Milk Tea"
-    },
-    {
-        id: 3,
-        name: "Strawberry Milk Tea",
-        description: "Pour prepared tea into each glass topped with 1 cup of strawberry milk.",
-        price: 42000,
-        thumbnail: require("../assets/images/strawberry-mt.png"),
-        category: "Milk Tea"
-    },
-    {
-        id: 4,
-        name: "Ice Lemon Tea",
-        description: "Make up a jug of refreshing iced tea to serve on summer days.",
-        price: 49000,
-        thumbnail: require("../assets/images/ice-lemon.png"),
-        category: "Specialtea"
-    },
-    {
-        id: 5,
-        name: "Berry Smoothie",
-        description: "Fresh or frozen fruit? For smoothies, frozen fruit is best.",
-        price: 32000,
-        thumbnail: require("../assets/images/berry-smoothie.png"),
-        category: "Smoothie"
-    },
-    {
-        id: 6,
-        name: "Immune Booster",
-        description: "Healthy Smoothies to Boost Your Immune System",
-        price: 38000,
-        thumbnail: require("../assets/images/immune-smoothie.png"),
-        category: "Smoothie"
-    },
-    {
-        id: 7,
-        name: "Very Berry",
-        description: "Fresh or frozen fruit? For smoothies, frozen fruit is best.",
-        price: 45000,
-        thumbnail: require("../assets/images/very-berry.png"),
-        category: "Smoothie"
-    },
-    {
-        id: 8,
-        name: "Watermelon Lychee Crush",
-        description: "Deliciously refreshing 2-ingredient watermelon smoothie. ",
-        price: 52000,
-        thumbnail: require("../assets/images/watermelon.png"),
-        category: "Smoothie"
-    },
-    {
-        id: 9,
-        name: "Vietnamese Traditional Coffee",
-        description: "The ubiquitous Vietnamese street coffee stalls utilize aluminum drip filters.",
-        price: 29000,
-        thumbnail: require("../assets/images/Vietnamese-traditional-coffee.png"),
-        category: "Coffee"
-    },
-    {
-        id: 10,
-        name: "Cappuccino",
-        description: "A cappuccino is an espresso-based coffee drink that originated in Italy.",
-        price: 32000,
-        thumbnail: require("../assets/images/latte.png"),
-        category: "Coffee"
-    },
-    {
-        id: 11,
-        name: "Mocha",
-        description: "Caffè mocha, also called mocaccino, is a chocolate-flavoured variant of a latte.",
-        price: 35000,
-        thumbnail: require("../assets/images/mocha.png"),
-        category: "Coffee"
-    },
-    {
-        id: 12,
-        name: "Espresso",
-        description: "Espresso is a coffee-brewing method of Italian origin.",
-        price: 35000,
-        thumbnail: require("../assets/images/espresso.png"),
-        category: "Coffee"
-    },
-    {
-        id: 13,
-        name: "Americano",
-        description: "Caffè Americano is a type of coffee drink prepared by an espresso with hot water.",
-        price: 32000,
-        thumbnail: require("../assets/images/americano.png"),
-        category: "Coffee"
-    },
-    {
-        id: 14,
-        name: "Hash Brown",
-        description: "Hash browns, also spelled hashed browns, are a popular American breakfast dish.",
-        price: 19000,
-        thumbnail: require("../assets/images/hash-brown.png"),
-        category: "Snack"
-    },
-    {
-        id: 15,
-        name: "French Fries",
-        description: "French-fried potatoes, are batonnet or allumette-cut deep-fried potatoes.",
-        price: 15000,
-        thumbnail: require("../assets/images/fries.png"),
-        category: "Snack"
-    },
+  {
+    id: 1,
+    name: 'Brown Sugar Fresh Milk Tea',
+    description: 'This drink is made from 3 ingredients, caramelised brown sugar and milk.',
+    price: 35000,
+    thumbnail: require('../assets/images/brown-sugar-fresh-mt.png'),
+    category: 'Milk Tea'
+  },
+  {
+    id: 2,
+    name: 'Matcha Milk Tea',
+    description: 'In a glass jar, mix the matcha green tea powder with a little bit of water.',
+    price: 39000,
+    thumbnail: require('../assets/images/matcha-mt.png'),
+    category: 'Milk Tea'
+  },
+  {
+    id: 3,
+    name: 'Strawberry Milk Tea',
+    description: 'Pour prepared tea into each glass topped with 1 cup of strawberry milk.',
+    price: 42000,
+    thumbnail: require('../assets/images/strawberry-mt.png'),
+    category: 'Milk Tea'
+  },
+  {
+    id: 4,
+    name: 'Ice Lemon Tea',
+    description: 'Make up a jug of refreshing iced tea to serve on summer days.',
+    price: 49000,
+    thumbnail: require('../assets/images/ice-lemon.png'),
+    category: 'Specialtea'
+  },
+  {
+    id: 5,
+    name: 'Berry Smoothie',
+    description: 'Fresh or frozen fruit? For smoothies, frozen fruit is best.',
+    price: 32000,
+    thumbnail: require('../assets/images/berry-smoothie.png'),
+    category: 'Smoothie'
+  },
+  {
+    id: 6,
+    name: 'Immune Booster',
+    description: 'Healthy Smoothies to Boost Your Immune System',
+    price: 38000,
+    thumbnail: require('../assets/images/immune-smoothie.png'),
+    category: 'Smoothie'
+  },
+  {
+    id: 7,
+    name: 'Very Berry',
+    description: 'Fresh or frozen fruit? For smoothies, frozen fruit is best.',
+    price: 45000,
+    thumbnail: require('../assets/images/very-berry.png'),
+    category: 'Smoothie'
+  },
+  {
+    id: 8,
+    name: 'Watermelon Lychee Crush',
+    description: 'Deliciously refreshing 2-ingredient watermelon smoothie. ',
+    price: 52000,
+    thumbnail: require('../assets/images/watermelon.png'),
+    category: 'Smoothie'
+  },
+  {
+    id: 9,
+    name: 'Vietnamese Traditional Coffee',
+    description: 'The ubiquitous Vietnamese street coffee stalls utilize aluminum drip filters.',
+    price: 29000,
+    thumbnail: require('../assets/images/Vietnamese-traditional-coffee.png'),
+    category: 'Coffee'
+  },
+  {
+    id: 10,
+    name: 'Cappuccino',
+    description: 'A cappuccino is an espresso-based coffee drink that originated in Italy.',
+    price: 32000,
+    thumbnail: require('../assets/images/latte.png'),
+    category: 'Coffee'
+  },
+  {
+    id: 11,
+    name: 'Mocha',
+    description: 'Caffè mocha, also called mocaccino, is a chocolate-flavoured variant of a latte.',
+    price: 35000,
+    thumbnail: require('../assets/images/mocha.png'),
+    category: 'Coffee'
+  },
+  {
+    id: 12,
+    name: 'Espresso',
+    description: 'Espresso is a coffee-brewing method of Italian origin.',
+    price: 35000,
+    thumbnail: require('../assets/images/espresso.png'),
+    category: 'Coffee'
+  },
+  {
+    id: 13,
+    name: 'Americano',
+    description: 'Caffè Americano is a type of coffee drink prepared by an espresso with hot water.',
+    price: 32000,
+    thumbnail: require('../assets/images/americano.png'),
+    category: 'Coffee'
+  },
+  {
+    id: 14,
+    name: 'Hash Brown',
+    description: 'Hash browns, also spelled hashed browns, are a popular American breakfast dish.',
+    price: 19000,
+    thumbnail: require('../assets/images/hash-brown.png'),
+    category: 'Snack'
+  },
+  {
+    id: 15,
+    name: 'French Fries',
+    description: 'French-fried potatoes, are batonnet or allumette-cut deep-fried potatoes.',
+    price: 15000,
+    thumbnail: require('../assets/images/fries.png'),
+    category: 'Snack'
+  }
 ]
 
 export const milkList = [
-    {
-        id: 1,
-        name: "No milk",
-        price: 0,
-        image: require("../assets/icons/milk3.png"),
-    },
-    {
-        id: 2,
-        name: "Ong Tho milk",
-        price: 18000,
-        image: require("../assets/icons/ong-tho-milk.png"),
-    },
-    {
-        id: 3,
-        name: "Vinamilk",
-        price: 9000,
-        image: require("../assets/icons/vinamilk.png"),
-    },
-    {
-        id: 4,
-        name: "TH True milk",
-        price: 10000,
-        image: require("../assets/icons/th-true-milk.png"),
-    }
+  {
+    id: 1,
+    name: 'No milk',
+    price: 0,
+    image: require('../assets/icons/milk3.png')
+  },
+  {
+    id: 2,
+    name: 'Ong Tho milk',
+    price: 18000,
+    image: require('../assets/icons/ong-tho-milk.png')
+  },
+  {
+    id: 3,
+    name: 'Vinamilk',
+    price: 9000,
+    image: require('../assets/icons/vinamilk.png')
+  },
+  {
+    id: 4,
+    name: 'TH True milk',
+    price: 10000,
+    image: require('../assets/icons/th-true-milk.png')
+  }
 ]
 
 const promos = [
-    {
-        id: 1,
-        name: "Goose Berry Smoothie",
-        description: "Goose Berry Smoothie with strawberry bits",
-        calories: "379 - 571",
-        image: require("../assets/images/goose-berry.png")
-    },
-    {
-        id: 2,
-        name: "Peach Tea",
-        description: "The best drink of the day",
-        calories: "400 - 570",
-        image: require("../assets/images/peach-tea.png")
-    },
-    {
-        id: 3,
-        name: "Black Tea Macchiato",
-        description: "Hot deal for you",
-        calories: "449 - 570",
-        image: require("../assets/images/black-tea.png")
-    },
+  {
+    id: 1,
+    name: 'Goose Berry Smoothie',
+    description: 'Goose Berry Smoothie with strawberry bits',
+    calories: '379 - 571',
+    image: require('../assets/images/goose-berry.png')
+  },
+  {
+    id: 2,
+    name: 'Peach Tea',
+    description: 'The best drink of the day',
+    calories: '400 - 570',
+    image: require('../assets/images/peach-tea.png')
+  },
+  {
+    id: 3,
+    name: 'Black Tea Macchiato',
+    description: 'Hot deal for you',
+    calories: '449 - 570',
+    image: require('../assets/images/black-tea.png')
+  }
 
 ]
 
 const dummyData = {
-    availableRewards,
-    locations,
-    menuList,
-    milkList,
-    promos,
-};
+  availableRewards,
+  locations,
+  menuList,
+  milkList,
+  promos
+}
 
-export default dummyData;
+export default dummyData
