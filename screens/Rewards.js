cbfa170b39e6801b1683bbf5268796f1857353d3
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 11:39:36 +0700

    eslint fix

--- screens/Rewards.js
+++ screens/Rewards.js
@@ -1,51 +1,51 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react'
 import {
-    View,
-    Text,
-    StyleSheet,
-    FlatList,
-    ImageBackground
-} from 'react-native';
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ImageBackground
+} from 'react-native'
 
-import { HeaderBar, CustomButton } from "../components";
+import { HeaderBar, CustomButton } from '../components'
 
-import { 
-    dummyData,
-    FONTS,
-    COLORS,
-    SIZES,
-    icons
-} from "../constants";
+import {
+  dummyData,
+  FONTS,
+  COLORS,
+  SIZES,
+  icons
+} from '../constants'
 
-import { connect } from "react-redux";
+import { connect } from 'react-redux'
 
 const Rewards = ({ navigation, appTheme }) => {
-
-    function renderRewardPointSection () {
-        return (
+  function renderRewardPointSection () {
+    return (
             <View
                 style={{
-                    alignItems: 'center',
-                    marginVertical: SIZES.padding
+                  alignItems: 'center',
+                  marginVertical: SIZES.padding
                 }}
             >
                 <Text
                     style={{
-                        color: COLORS.primary,
-                        ...FONTS.h1,
-                        fontSize: 35
+                      color: COLORS.primary,
+                      ...FONTS.h1,
+                      fontSize: 35
                     }}
                 >
                     Rewards
                 </Text>
                 <Text
                     style={{
-                        color: appTheme.textColor,
-                        width: SIZES.width * 0.6,
-                        textAlign: 'center',
-                        ...FONTS.h3,
-                        lineHeight: 18,
-                        marginTop: 10
+                      color: appTheme.textColor,
+                      width: SIZES.width * 0.6,
+                      textAlign: 'center',
+                      ...FONTS.h3,
+                      lineHeight: 18,
+                      marginTop: 10
                     }}
                 >
                     You are 60 points away from your next reward
@@ -55,93 +55,93 @@ const Rewards = ({ navigation, appTheme }) => {
                     source={icons.reward_cup}
                     resizeMode='contain'
                     style={{
-                        marginTop: SIZES.padding,
-                        width: SIZES.width * 0.8,
-                        height: SIZES.width * 0.8,
-                        alignItems: 'center',
-                        justifyContent: 'center'
+                      marginTop: SIZES.padding,
+                      width: SIZES.width * 0.8,
+                      height: SIZES.width * 0.8,
+                      alignItems: 'center',
+                      justifyContent: 'center'
                     }}
                 >
                     <View
                         style={{
-                            width: 70,
-                            height: 70,
-                            borderRadius: 35,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            backgroundColor: COLORS.white
+                          width: 70,
+                          height: 70,
+                          borderRadius: 35,
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          backgroundColor: COLORS.white
                         }}
                     >
                         <Text style={{ ...FONTS.h1 }}>199</Text>
                     </View>
                 </ImageBackground>
             </View>
-        )
-    }
+    )
+  }
 
-    function renderButtons () {
-        return (
+  function renderButtons () {
+    return (
             <View
                 style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'center'
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center'
                 }}
             >
-                <CustomButton 
+                <CustomButton
                     isPrimaryButton={true}
                     label='Scan in store'
                     containerStyle={{
-                        width: 130,
-                        paddingVertical: 5,
-                        marginRight: SIZES.radius,
-                        borderRadius: SIZES.radius * 2
+                      width: 130,
+                      paddingVertical: 5,
+                      marginRight: SIZES.radius,
+                      borderRadius: SIZES.radius * 2
                     }}
                     labelStyle={{ ...FONTS.h3 }}
                     onPress={() => navigation.navigate('Location')}
                 />
 
-                <CustomButton 
+                <CustomButton
                     isSecondaryButton={true}
                     label='Redeem'
                     containerStyle={{
-                        width: 130,
-                        paddingVertical: 5,
-                        borderRadius: SIZES.radius * 2
+                      width: 130,
+                      paddingVertical: 5,
+                      borderRadius: SIZES.radius * 2
                     }}
                     labelStyle={{ ...FONTS.h3 }}
                     onPress={() => navigation.navigate('Location')}
                 />
             </View>
-        )
-    }
+    )
+  }
 
-    function renderAvailableRewardHeader () {
-        return (
+  function renderAvailableRewardHeader () {
+    return (
             <View
                 style={{
-                    marginTop: SIZES.padding,
-                    marginBottom: SIZES.radius,
-                    paddingHorizontal: SIZES.padding
+                  marginTop: SIZES.padding,
+                  marginBottom: SIZES.radius,
+                  paddingHorizontal: SIZES.padding
                 }}
             >
                 <Text style={{ color: appTheme.textColor, ...FONTS.h2 }}>Available Rewards</Text>
             </View>
-        ) 
-    }
+    )
+  }
 
-    return (
+  return (
         <View style={styles.container}>
             {/* Header */}
             <HeaderBar />
 
             {/* Details */}
-            <FlatList 
+            <FlatList
                 style={{
-                    marginTop: -25,
-                    borderTopLeftRadius: SIZES.radius * 2,
-                    borderTopRightRadius: SIZES.radius * 2,
-                    backgroundColor: appTheme.backgroundColor
+                  marginTop: -25,
+                  borderTopLeftRadius: SIZES.radius * 2,
+                  borderTopRightRadius: SIZES.radius * 2,
+                  backgroundColor: appTheme.backgroundColor
                 }}
                 data={dummyData.availableRewards}
                 keyExtractor={item => `${item.id}`}
@@ -158,51 +158,51 @@ const Rewards = ({ navigation, appTheme }) => {
                         {renderAvailableRewardHeader()}
                     </View>
                 }
-                renderItem={({item}) => {
-                    return (
+                renderItem={({ item }) => {
+                  return (
                         <View
                             style={{
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                marginHorizontal: SIZES.padding,
-                                marginBottom: SIZES.base,
-                                paddingVertical: SIZES.base,
-                                borderRadius: 20,
-                                backgroundColor: item.eligible ? COLORS.yellow : COLORS.gray2
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                              marginHorizontal: SIZES.padding,
+                              marginBottom: SIZES.base,
+                              paddingVertical: SIZES.base,
+                              borderRadius: 20,
+                              backgroundColor: item.eligible ? COLORS.yellow : COLORS.gray2
                             }}
                         >
                             <Text
                                 style={{
-                                    color: item.eligible ? COLORS.black : COLORS.lightGray2,
-                                    ...FONTS.body3
+                                  color: item.eligible ? COLORS.black : COLORS.lightGray2,
+                                  ...FONTS.body3
                                 }}
                             >
                                 {item.title}
                             </Text>
                         </View>
-                    )
+                  )
                 }}
                 ListFooterComponent={
                     <View style={{ marginBottom: 120 }} />
                 }
             />
         </View>
-    )
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+  container: {
+    flex: 1
+  }
 })
 
 // export default Rewards;
 
 function mapStateToProps (state) {
-    return {
-        appTheme: state.appTheme,
-        error: state.error
-    }
+  return {
+    appTheme: state.appTheme,
+    error: state.error
+  }
 }
 
-export default connect(mapStateToProps)(Rewards);
+export default connect(mapStateToProps)(Rewards)
