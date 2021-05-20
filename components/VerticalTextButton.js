f3f16fee4af64a72d4173be9ef26f5b796b79a03
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 11:43:25 +0700

    eslint fix

--- components/VerticalTextButton.js
+++ components/VerticalTextButton.js
@@ -1,34 +1,35 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React from 'react'
 
-import { 
-    TouchableOpacity,
-    Text
-} from "react-native";
+import {
+  TouchableOpacity,
+  Text
+} from 'react-native'
 
-import { FONTS, SIZES, COLORS } from "../constants";
+import { FONTS, COLORS } from '../constants'
 
 const VerticalTextButton = ({ containerStyle, label, selected, onPress }) => {
-    return (
+  return (
         <TouchableOpacity
             style={{
-                alignItems: 'center',
-                transform: [{ rotate: '-90deg' }],
-                ...containerStyle
+              alignItems: 'center',
+              transform: [{ rotate: '-90deg' }],
+              ...containerStyle
             }}
             onPress={onPress}
         >
             <Text
                 style={{
-                    color: selected ? COLORS.white : COLORS.yellow,
-                    ...FONTS.body2,
-                    fontSize: selected ? 20 : 16,
-                    fontWeight: selected ? "bold" : 'normal'
+                  color: selected ? COLORS.white : COLORS.yellow,
+                  ...FONTS.body2,
+                  fontSize: selected ? 20 : 16,
+                  fontWeight: selected ? 'bold' : 'normal'
                 }}
             >
                 {label}
             </Text>
         </TouchableOpacity>
-    )
+  )
 }
 
-export default VerticalTextButton;
+export default VerticalTextButton
