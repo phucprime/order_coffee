d6487c06466ac47fb66935db9994efec2618853a
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 11:39:07 +0700

    eslint fix

--- stores/themeActions.js
+++ stores/themeActions.js
@@ -1,38 +1,38 @@
-import { lightTheme, darkTheme } from "../constants";
+import { lightTheme, darkTheme } from '../constants'
 
 export const TOGGLE_THEME_BEGIN = 'TOGGLE_THEME_BEGIN'
 export const TOGGLE_THEME_SUCCESS = 'TOGGLE_THEME_SUCCESS'
 export const TOGGLE_THEME_FAILURE = 'TOGGLE_THEME_FAILURE'
 
 export const toggleThemeBegin = () => ({
-    type: TOGGLE_THEME_BEGIN
+  type: TOGGLE_THEME_BEGIN
 })
 
 export const toggleThemeSuccess = (selectedTheme) => ({
-    type: TOGGLE_THEME_SUCCESS,
-    payload: { selectedTheme }
+  type: TOGGLE_THEME_SUCCESS,
+  payload: { selectedTheme }
 })
 
 export const toggleThemeFailure = error => ({
-    type: TOGGLE_THEME_FAILURE,
-    payload: { error }
+  type: TOGGLE_THEME_FAILURE,
+  payload: { error }
 })
 
 export function toggleTheme (themeType) {
-    return dispatch => {
-        dispatch(toggleThemeBegin())
+  return dispatch => {
+    dispatch(toggleThemeBegin())
 
-        switch (themeType) {
-            case "dark":
-                dispatch(toggleThemeSuccess(darkTheme))
-                break;
-            case "light":
-                dispatch(toggleThemeSuccess(lightTheme))
-                break;
-        
-            default:
-                dispatch(toggleThemeFailure({ error: "Invalid theme type" }))
-                break;
-        }
+    switch (themeType) {
+      case 'dark':
+        dispatch(toggleThemeSuccess(darkTheme))
+        break
+      case 'light':
+        dispatch(toggleThemeSuccess(lightTheme))
+        break
+
+      default:
+        dispatch(toggleThemeFailure({ error: 'Invalid theme type' }))
+        break
     }
+  }
 }
