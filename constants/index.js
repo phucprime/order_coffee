d2f86477d1c99ad91da96dde684862c9b29f3552
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 11:41:39 +0700

    eslint fix

--- constants/index.js
+++ constants/index.js
@@ -1,19 +1,19 @@
-import constants from "./constants";
-import dummyData from "./dummy";
-import icons from "./icons";
-import images from "./images";
-import theme, { COLORS, SIZES, FONTS, darkTheme, lightTheme, selectedTheme } from "./theme";
+import constants from './constants'
+import dummyData from './dummy'
+import icons from './icons'
+import images from './images'
+import theme, { COLORS, SIZES, FONTS, darkTheme, lightTheme, selectedTheme } from './theme'
 
 export {
-    constants,
-    dummyData,
-    icons,
-    images,
-    theme,
-    COLORS,
-    SIZES,
-    FONTS,
-    darkTheme,
-    lightTheme,
-    selectedTheme
-};
+  constants,
+  dummyData,
+  icons,
+  images,
+  theme,
+  COLORS,
+  SIZES,
+  FONTS,
+  darkTheme,
+  lightTheme,
+  selectedTheme
+}
