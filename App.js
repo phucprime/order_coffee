e5c142661570cd0c9948f4d4ad637d085d6f28b8
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 11:36:44 +0700

    eslint fix

--- App.js
+++ App.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import { Location, Order, OrderDetail } from "./screens";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react'
+import { Location, Order, OrderDetail } from './screens'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
 import SplashScreen from 'react-native-splash-screen'
 
-import Tabs from "./navigation/tabs";
+import Tabs from './navigation/tabs'
 
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import themeReducer from "./stores/themeReducer";
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import themeReducer from './stores/themeReducer'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
-const store = createStore (
-    themeReducer,
-    applyMiddleware(thunk)
+const store = createStore(
+  themeReducer,
+  applyMiddleware(thunk)
 )
 
 const App = () => {
+  React.useEffect(() => {
+    SplashScreen.hide()
+  }, [])
 
-    React.useEffect(() => {
-        SplashScreen.hide();
-    }, [])
-
-    return (
+  return (
         <Provider store={store}>
             <NavigationContainer>
                 <Stack.Navigator
                     screenOptions={{
-                        headerShown: false
+                      headerShown: false
                     }}
                     initialRouteName={'Home'}
                 >
@@ -55,7 +54,7 @@ const App = () => {
                 </Stack.Navigator>
             </NavigationContainer>
         </Provider>
-    )
+  )
 }
 
-export default App;
+export default App
