bb27e4eda6383b8c5b2cffa62d47cfb1a741cc24
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 11:37:44 +0700

    eslint fix

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    project: {
-      ios: {},
-      android: {}, // grouped into "project"
-    },
-    assets: ["./assets/fonts/"],
-  };
+  project: {
+    ios: {},
+    android: {} // grouped into "project"
+  },
+  assets: ['./assets/fonts/']
+}
