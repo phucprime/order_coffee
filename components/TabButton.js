8df2758aabd8e6e0b745f389e0d04b6ee073c58e
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-05-20 11:43:42 +0700

    eslint fix

--- components/TabButton.js
+++ components/TabButton.js
@@ -1,44 +1,44 @@
-import React from "react";
+import React from 'react'
 
-import { 
-    TouchableOpacity,
-    View,
-    Text
-} from "react-native";
+import {
+  TouchableOpacity,
+  View,
+  Text
+} from 'react-native'
 
-import { COLORS, FONTS } from "../constants";
+import { COLORS, FONTS } from '../constants'
 
 const TabButton = ({ containerStyle, label, selected, onPress }) => {
-    return (
+  return (
         <TouchableOpacity
             style={{
-                alignItems: 'center',
-                ...containerStyle
+              alignItems: 'center',
+              ...containerStyle
             }}
             onPress={onPress}
         >
             {/* Text */}
             <Text
                 style={{
-                    color: selected ? COLORS.primary : COLORS.gray,
-                    ...FONTS.body2,
-                    fontSize: 18
+                  color: selected ? COLORS.primary : COLORS.gray,
+                  ...FONTS.body2,
+                  fontSize: 18
                 }}
             >
                 {label}
             </Text>
 
             {/* Underline */}
-            <View 
+            <View
                 style={{
-                    marginTop: selected ? 3 : 4,
-                    height: selected ? 4 : 2,
-                    width: '100%',
-                    backgroundColor: selected ? COLORS.primary : COLORS.gray
+                  marginTop: selected ? 3 : 4,
+                  height: selected ? 4 : 2,
+                  width: '100%',
+                  backgroundColor: selected ? COLORS.primary : COLORS.gray
                 }}
             />
         </TouchableOpacity>
-    )
+  )
 }
 
-export default TabButton;
+export default TabButton
